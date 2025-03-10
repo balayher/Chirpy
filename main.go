57115c7b5a4fec90afc82515aecd17707631b1c9
balayher <balayher@gmail.com>
2025-03-10 10:24:44 -0700

    creating local server

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main() {
+	ServeMux := http.NewServeMux()
+	server := http.Server{
+		Handler: ServeMux,
+		Addr:    ":8080",
+	}
+	server.ListenAndServe()
+}
